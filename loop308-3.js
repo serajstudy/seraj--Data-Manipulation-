ca7f910c380b7f1e0ddf1a35c46552c2b15b1c3c
seraj <serajstudy1@gmail.com>
2025-08-04 11:17:17 -0400

    add loop

--- loop308-3.js
+++ loop308-3.js
@@ -1,20 +1,75 @@
 
-for( let i= 1; i<=100; i++){
+// for( let i= 1; i<=100; i++){
 
-    if ( i % 3 === 0){
-        console.log("Fizz");
+//     if ( i % 3 === 0){
+//         console.log("Fizz");
 
-    } else if ( i % 5 ===0){
-        console.log("Buzz");
-    } else  if ( i % 3===0 && i % 5===0){
-        console.log (" FizzBuzz");
-    } else {console.log(i)
+//     } else if ( i % 5 ===0){
+//         console.log("Buzz");
+//     } else  if ( i % 3===0 && i % 5===0){
+//         console.log (" FizzBuzz");
+//     } else {console.log(i)
 
-    }
+//     }
  
+// }
+
+
+// Part 3: Feeling Loopy
+
+
+
+
+// // CSV data looks like this:
+// ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
+
+
+// Starting String
+let csvStr = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26`;
+
+//  Variables
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
+let commas = 0; // Counter variable
+
+// Loop through string, for loop to have access to the index
+for (let i = 0; i < csvStr.length; i++) {
+  if (csvStr[i] == ",") {
+    // If comma
+    commas++; // Add to comma counter
+  } else if (csvStr[i] == "\n") {
+    // If new line
+    console.log(cell1, cell2, cell3, cell4); // print new line
+
+    // Clear previous data for new row
+    commas = 0;
+    cell1 = "";
+    cell2 = "";
+    cell3 = "";
+    cell4 = "";
+  } else {
+    // for all chars
+    if (commas == 0) {
+      // put in the appropriate cell, based on commas
+      cell1 += csvStr[i];
+    } else if (commas == 1) {
+      cell2 += csvStr[i];
+    } else if (commas == 2) {
+      cell3 += csvStr[i];
+    } else {
+      cell4 += csvStr[i];
+    }
+  }
+
+  // If last char in string print final cells
+  if (csvStr.length - 1 == i) {
+    console.log(cell1, cell2, cell3, cell4);
+  }
 }
 
-
+//  How can I tell when a cell ends? commas!!!
 
 
 
